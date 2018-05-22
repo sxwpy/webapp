ce1da629644df89f748b0a59cbd53ca8ff74e657
songxiaowei <1073566584@qq.com>
2018-05-22 20:53:48 +0800

    add app.py

---
+++ www/app.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/5/22 20:40
+# @Author  : sxwpy
+# @FileName: app.py
+# @Software: PyCharm
+'''
+async web application
+'''
+
+import logging;logging.basicConfig(level=logging.INFO)
+import asyncio,os,json,time
+from datetime import datetime
+from aiohttp import web
+def index(request):
+    return  web.Response(body=b'<h1>webapp</h1>',content_type='text/html')
+
+async def init(loop):
+    app=web.Application(loop=loop)
+    app.router.add_route('GET','/',index)
+    srv=await loop.create_server(app.make_handler(),'127.0.0.1',9000)
+    logging.info('server started at http://127.0.0.1:9000...')
+    return  srv
+
+loop=asyncio.get_event_loop()
+loop.run_until_complete((init(loop)))
+loop.run_forever()
