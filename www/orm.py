059e5186ebaf32e93ce2850b0b8c557f8a81bf98
sxwpy <1073566584@qq.com>
2018-05-23 10:16:06 +0800

    complete orm.py

--- www/orm.py
+++ www/orm.py
@@ -107,15 +107,15 @@ class MOdelMetaclass(type):
                 if v.primary_key:
                     #找到主键
                     if primarykey:
-                        raise Exception('Duplicate primary key for field :%s'%k)
+                        raise RuntimeError('Duplicate primary key for field :%s'%k)
                     primarykey=k
                 else:
                     fields.append(k)
         if not primarykey:
-            raise Exception('primary key not found')
+            raise ReferenceError('primary key not found')
         for k in mappings.keys():
             attrs.pop(k)
-        escaped_fields=list(map(lambda f:'`%s`' % f, fields))
+        escaped_fields=list(map(lambda f:'`%s`' %f, fields))
         attrs['__mappings__']=mappings
         attrs['__table__']=tableName
         attrs['__primary_key__']=primarykey
@@ -126,3 +126,88 @@ class MOdelMetaclass(type):
         tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primarykey)
         attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primarykey)
         return type.__new__(cls, name, bases, attrs)
+
+class Model(dict,metaclass=ModelMetaclass):
+	def __init__(self,**kw):
+	super(Model,self).__init__(**kw)
+	
+	def __getattr__(self,key):
+		try:
+			return self[key]
+		except KeyError:
+			raise AttributeError(r"'Model' object has no attribute '%s'"%key)
+		
+	def __setattr__(sef,key,value):
+		self[key]=value
+		
+	def getValue(self,key):
+		return getattr(self,key,None)
+		
+	def getValueOrDefault(self,key):
+		value=getattr(self,key,None)
+		if value is None:
+			field =self.__mappings__[key]
+			if field.defaul is not None:
+				value=field.default() if callable(field.default) else field.default
+				logging.debug('using default value for %s:%s'%(key,str(value)))
+				setattr(self,key,value)
+		return value
+		
+	@classmethod
+	async def findAll(cls,where=None,args=None,**kw):
+		'find objects by where clause'
+		sql=[cls.__select__]
+		if where :
+			sql.append('where')
+			sql.append(where)
+		if args is None:
+			args=[]
+		orderBy=kw.get('orderBy',None)
+		if orderBy:
+			sql.append('orderBy')
+			sql.append(orderBy)
+		limit=kw.get('limit',None)
+		if limit is not None:
+			sql.append('limit')
+			if isinstance(limit,int):
+				sql.append('?')
+				args.append(limit)
+			elif isinstance(limit,tuple) and len(limit)==2:
+				sql.append('?','?')
+				args.extend(limit)
+			else:
+				raise ValueError('Invalid limit value:%s'%str(limit))
+		rs =await select(' '.join(sql),args)
+		return [cls(**r) for r in rs]
+		
+	@classmethod
+	async def findNumber(cls,selectField,where=None,args=None):
+		'find number by select and where.'
+		sql=['select %s _num_ from `%s`'%(selectField,cls.__table__)]
+		if where:
+			sql.append('where')
+			sql.append(where)
+		rs= await select(' '.join(sql),args,1)
+		if len(rs)==0
+			return None
+		return cls(**rs[0])
+		
+	async def save(self):
+		args=list(map(self.getValueOrDefault,self.__fields__))
+		args.append(self.getValueOrDefault(self.__primary_key__))
+		rows=await execute(self.__init__,args)
+		if rows!=1:
+			logging.warn('failed to insert record:affected row:%s'%rows)
+				
+	async def update(self):
+		args=list(map(self.getValue,self.__fields__))
+		args.append(self.getValue(self.__primary_key__))
+		rows=await execute(self.__update__,args)
+		if rows!=1:
+			logging.warn('failed to update by primary key : affected rows:%s'%rows)
+			
+	async def remove(self)
+		args=[self.getValue(self.__primary_key__)]
+		rows=await execute(self.__delete__,args)
+		if rows!=1:
+			logging.warn('failed to remove by primary key:affected rows:%s'%rows)
